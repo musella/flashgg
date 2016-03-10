7006de4fa6eaaedcf2a78b297da5f2e6d13d3024
Pasquale Musella <pasquale.musella@cern.ch>
2016-03-10 09:22:01 +0100

    shorten job names

--- MetaData/work/prepareCrabJobs.py
+++ MetaData/work/prepareCrabJobs.py
@@ -253,6 +253,20 @@ if options.createCrabConfig:
         while os.path.isdir("crab_" + jobname):
             itry += 1
             jobname = "_".join([flashggVersion, PrimaryDataset, ProcessedDataset, str(itry).zfill(2)])
+        if len(jobname) > 100:
+            jobname = jobname.replace("TuneCUEP8M1_13TeV-pythia8","13TeV")
+        if len(jobname) > 100:
+            jobname = jobname.replace("TuneCUETP8M1_13TeV-madgraphMLM-pythia8","13TeV-mg")
+        if len(jobname) > 100:
+            jobname = jobname.replace("RSGravToGG","Grav")
+        if len(jobname) > 100:
+            jobname = jobname.replace("-PU25nsData2015v1","")
+        if len(jobname) > 100:
+            jobname = jobname.replace("RunIIFall15MiniAODv2_","")
+        if len(jobname) > 100:
+            jobname = jobname.replace("RunIIFall15MiniAODv2-magnetOffBS0T_PU25nsData2015v1_0T","0T")
+        if len(jobname) > 100:
+            print "jobname length: %d " % len(jobname)
         # Actually create the config file: copy the template and replace things where appropriate
         crabConfigFile = "crabConfig_" + jobname + ".py"
         print "Preparing crab for processing ", PrimaryDataset, "\n      -> ", crabConfigFile
